4a48c2488c77429ec3e89e0030a8c78feabe20af
Alexandra <Alexandra.kunitskaya@gmail.com>
2019-10-11 11:10:10 +0300

    init: start implementing guessing-game binary search algorithm

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,36 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.left = {min, max};
+        this.right = {min, max};
+        this.medium = 0;
     }
 
     guess() {
-
+        this.medium = parseInt(this.right.max / 2 + 1, 10);
+        return this.medium;
     }
 
     lower() {
+        this.right.max = this.medium;
+        this.medium = parseInt(this.right.max / 2 + 1, 10);
+        this.right.min = this.medium + 1;
 
+        this.left.max = this.medium - 1;
+
+        return this.medium;
     }
 
     greater() {
+        this.left.min = this.medium;
+        this.medium = parseInt(this.right.max / 2 + 1, 10);
+        this.left.max = this.medium - 1;
 
+        this.right.min = this.medium + 1;
+
+        return this.medium;
     }
 }
 
