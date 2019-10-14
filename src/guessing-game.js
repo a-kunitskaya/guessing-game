d2aab8a8157d552f193d9d9bd2ab23ea2e5e091d
Alexandra <Alexandra.kunitskaya@gmail.com>
2019-10-14 07:05:48 +0300

    feat: implement biary search algrithm

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -3,34 +3,22 @@ class GuessingGame {
     }
 
     setRange(min, max) {
-        this.left = {min, max};
-        this.right = {min, max};
+        this.min = min;
+        this.max = max;
         this.medium = 0;
     }
 
     guess() {
-        this.medium = parseInt(this.right.max / 2 + 1, 10);
+        this.medium = Math.round((this.min + this.max) / 2);
         return this.medium;
     }
 
     lower() {
-        this.right.max = this.medium;
-        this.medium = parseInt(this.right.max / 2 + 1, 10);
-        this.right.min = this.medium + 1;
-
-        this.left.max = this.medium - 1;
-
-        return this.medium;
+        this.max = this.medium;
     }
 
     greater() {
-        this.left.min = this.medium;
-        this.medium = parseInt(this.right.max / 2 + 1, 10);
-        this.left.max = this.medium - 1;
-
-        this.right.min = this.medium + 1;
-
-        return this.medium;
+        this.min = this.medium;
     }
 }
 
